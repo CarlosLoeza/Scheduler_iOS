bc2a84d8819c7a62b021809d468c83f64b78c571
“CarlosLoeza” <loeza.carlos@yahoo.com>
2021-06-19 15:23:18 -0700

    Set up code to create a POST request to send syllabus image to server. Server will apply Computer Vision to read and return dates and assignment names

--- Schedule/ViewController.swift
+++ Schedule/ViewController.swift
@@ -17,18 +17,73 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 
     // Work in progress: create a POST request
     @IBAction func send_image(_ sender: UIButton) {
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-                imageView.image = image
-            } else {
-                print("ERROR")
+        //call postRequest with username and password parameters
+        postRequest(username: "username", password: "password") { (result, error) in
+            if let result = result {
+                print("success: \(result)")
+            } else if let error = error {
+                print("error: \(error.localizedDescription)")
             }
-            dismiss(animated: true, completion: nil)
         }
     }
     
     
-    // Display image we selected
+    func postRequest(username: String, password: String, completion: @escaping ([String: Any]?, Error?) -> Void) {
+
+        //declare parameter as a dictionary which contains string as key and value combination.
+        let parameters = ["name": username, "password": password]
+
+        //create the url with NSURL
+        let url = URL(string: "http://167.172.157.65:5000")!
+
+        //create the session object
+        let session = URLSession.shared
+
+        //now create the Request object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to data object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+            completion(nil, error)
+        }
+
+        //HTTP Headers
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request, completionHandler: { data, response, error in
+
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+
+            guard let data = data else {
+                completion(nil, NSError(domain: "dataNilError", code: -100001, userInfo: nil))
+                return
+            }
+
+            do {
+                //create json object from data
+                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {
+                    completion(nil, NSError(domain: "invalidJSONTypeError", code: -100009, userInfo: nil))
+                    return
+                }
+                print(json)
+                completion(json, nil)
+            } catch let error {
+                print(error.localizedDescription)
+                completion(nil, error)
+            }
+        })
+        task.resume()
+    }
+    
+   
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = image
@@ -38,11 +93,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 
         dismiss(animated: true, completion: nil)
     }
-    
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
